7e5e971fc6ca52bd1071c0f42a08e664f4646f98
balogun michael <balogunmaleek2001@gmail.com>
2024-07-08 13:15:20 +0100

    Add fruit data and API endpoints for retrieving fruits

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 import express from "express"
+import data from './data/data.json' assert {type: "json"}
 
 
 const app = express()
 const PORT = 3000
+app.use(express.json());
+
+app.get("/", (req, res) => { {
+    res.send("Hello World!");
+  }
+})
+
+app.post("/fruits", (req, res) => {
+console.log(req.body);
+}
+)
+
+app.get("/fruits/:fruitName?", (req, res) => {
+  const {fruitName} = req.params;
+  if(fruitName) {
+    const fruit = data.find((f) => f.name === fruitName);
+    if(!fruit){
+      res.status(404).send("Fruit not found");
+    }
+    res.status(200).send(fruit);
+  }
+else if (!fruitName) {
+  res.status(200).send(data)
+}
+else if(!data) {
+  res.status(500).send("server error");
+}
+});
+
+app.get("/Users/:id?", (req, res) => {
+  if (req.params.id) {
+    res.send(`User ID: ${req.params.id}`);
+  } else {
+    res.send("All users");
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.get("/users", (req, res) => {
-    res.send("tobi.")
-  })
+
   
   app.get("/admin", (req, res) => {
     res.send("tobi4u.")
